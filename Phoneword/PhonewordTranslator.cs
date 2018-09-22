9d22cf1cf681ecfcfd969d32955eda2dfa2777e2
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-09-22 18:01:47 +0300

    Finish dealing with one-script mode. Didn't manage to change visual theme; Can't deploy release.

---
+++ Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Phoneword
+{
+	internal class PhonewordTranslator
+	{
+		public static string ToNumber(string raw)
+		{
+			if (String.IsNullOrWhiteSpace(raw))
+			{
+				return "";
+			}
+			else
+			{
+				raw = raw.ToUpperInvariant();
+			}
+
+			var newNumber = new StringBuilder();
+			var charsToSkip = " -0123456789";
+			foreach (var c in raw)
+			{
+				if (charsToSkip.Contains(c))
+				{
+					newNumber.Append(c);
+				}
+				else
+				{
+					var result = TranslateToNumber(c);
+
+					if (result != null)
+					{
+						newNumber.Append(result);
+					}
+				}
+
+				// otherwise we got a non-numeric char. And skipped that.
+
+				
+			}
+			return newNumber.ToString();
+		}
+
+		private static int? TranslateToNumber(char c)
+		{
+			if (char.IsLetter(c))
+			{
+				return c % 48 / 5;
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
